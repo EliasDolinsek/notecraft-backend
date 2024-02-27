0c6009ea1d16fb276072fe6795421d071ed1647a
eld <elias.dolinsek@gmail.com>
2024-02-27 19:31:42 +0100

    Updated imports

--- src/main/java/com/eliasdolinsek/notecraft/controller/AuthController.java
+++ src/main/java/com/eliasdolinsek/notecraft/controller/AuthController.java
@@ -1,35 +1,23 @@
 package com.eliasdolinsek.notecraft.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.eliasdolinsek.notecraft.models.ERole;
-import com.eliasdolinsek.notecraft.models.Role;
 import com.eliasdolinsek.notecraft.models.User;
 import com.eliasdolinsek.notecraft.payload.request.LoginRequest;
 import com.eliasdolinsek.notecraft.payload.request.SignupRequest;
 import com.eliasdolinsek.notecraft.payload.response.JwtResponse;
 import com.eliasdolinsek.notecraft.payload.response.MessageResponse;
-import com.eliasdolinsek.notecraft.repository.RoleRepository;
 import com.eliasdolinsek.notecraft.repository.UserRepository;
 import com.eliasdolinsek.notecraft.security.jwt.JwtUtils;
 import com.eliasdolinsek.notecraft.security.services.UserDetailsImpl;
 
-import io.jsonwebtoken.lang.Arrays;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,7 +30,6 @@ public class AuthController {
 
     private AuthenticationManager authenticationManager;
     private UserRepository userRepository;
-    private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
     private JwtUtils jwtUtils;
 
